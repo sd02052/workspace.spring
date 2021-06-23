21701bc137b436be051989e281bda4facd2349f7
sist <sist@DESKTOP-216D9JE>
2021-06-23 15:31:48 +0900

    di11

---
+++ 02_DI/src/main/java/com/sist/di11/Main.java
@@ -0,0 +1,37 @@
+package com.sist.di11;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+
+		Player player1 = (Player) ctx.getBean("player1");
+		System.out.println("이름 : " + player1.getName());
+		System.out.println("나이 : " + player1.getAge());
+		System.out.println("포지션 : " + player1.getPosition());
+		System.out.println("체중 : " + player1.getWeight());
+		System.out.println("키 : " + player1.getHeight());
+		System.out.println("===================================================");
+
+		Player player2 = (Player) ctx.getBean("player2");
+		System.out.println("이름 : " + player2.getName());
+		System.out.println("나이 : " + player2.getAge());
+		System.out.println("포지션 : " + player2.getPosition());
+		System.out.println("체중 : " + player2.getWeight());
+		System.out.println("키 : " + player2.getHeight());
+		System.out.println("===================================================");
+
+		Player player3 = (Player) ctx.getBean("player3");
+		System.out.println("이름 : " + player3.getName());
+		System.out.println("나이 : " + player3.getAge());
+		System.out.println("포지션 : " + player3.getPosition());
+		System.out.println("체중 : " + player3.getWeight());
+		System.out.println("키 : " + player3.getHeight());
+		System.out.println("===================================================");
+		ctx.close();
+	}
+}
